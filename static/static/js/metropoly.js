eed89e1eb9cdddb1ac206000e80e3813180dfe6f
Yohannaaaaa <k.svetlanaheves@gmail.com>
2026-06-15 11:33:21 +0200

    Update metropoly.js

--- static/static/js/metropoly.js
+++ static/static/js/metropoly.js
@@ -1,1 +1,14 @@
 
+console.log("Metropoly chargé");
+
+function rollDice(){
+    console.log("Dé lancé");
+}
+
+function createRoom(){
+    console.log("Créer salle");
+}
+
+function joinRoom(){
+    console.log("Rejoindre salle");
+}
